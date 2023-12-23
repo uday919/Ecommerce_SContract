3339cdb7c82ad4e7be24f0cb3b4f74cc7c6c1931
uday919 <udaykiran919p@gmail.com>
2023-12-23 15:38:02 -0600

    upload

---
+++ Ecommerce/Ecommerce.sol
@@ -0,0 +1,57 @@
+//SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.19;
+
+contract ecommerce{
+
+struct Product{ 
+    string title; 
+    string desc; 
+    address payable seller; 
+    uint productId; 
+    uint price; 
+    address buyer; 
+    bool delivered; 
+} 
+    address payable manager; 
+    bool destroyed=false; 
+    
+constructor(){ 
+        manager=payable(msg.sender); 
+} 
+        uint counter=1; 
+        
+        Product[] public products;
+
+modifier isNotDestroyed{ 
+    require(!destroyed,"Contract does not exist");
+     _; 
+     }
+
+function registerProduct(string memory _title,string memory _desc,uint _price) public isNotDestroyed{ 
+    require(_price>0,"Price should be greater than zero"); 
+    Product memory tempProduct; 
+    tempProduct.title =_title; 
+    tempProduct.desc=_desc; 
+    tempProduct.price=_price * 10**18; 
+    tempProduct.seller=payable(msg.sender); 
+    tempProduct.productId=counter; 
+    products.push(tempProduct); 
+    counter++; }
+
+function buy(uint _productId) payable public isNotDestroyed{ 
+    require(products[_productId-1].price==msg.value,"Pay the exact price"); 
+    require(products[_productId-1].seller!=msg.sender,"Seller cannot buy"); 
+    products[_productId-1].buyer=msg.sender; 
+    } 
+function delivery(uint _productId) public isNotDestroyed{ 
+    require(products[_productId-1].buyer==msg.sender,"Only buyer can confirm"); 
+    products[_productId-1].delivered=true; 
+    products[_productId-1].seller.transfer(products[_productId-1].price); }
+
+
+function destroy() public isNotDestroyed{ 
+    require(manager==msg.sender,"only manager can call this"); 
+    manager.transfer(address(this).balance); 
+    destroyed=true; 
+    }
+}
